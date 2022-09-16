ce6cbb798745bfaaf3d0e9d247e90b2e4647e2a0
rjquiroz02 <rjquiroz02@unitec.edu>
2022-09-16 13:52:21 -0600

    Commit1

---
+++ src/lab9p2_ricardoquiroz_rigobertobarahona/Lab9P2_RicardoQuiroz_RigobertoBarahona.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab9p2_ricardoquiroz_rigobertobarahona;
+
+/**
+ *
+ * @author rjqer
+ */
+public class Lab9P2_RicardoQuiroz_RigobertoBarahona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
